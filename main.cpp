6fa409c452d3bd40a0cbe740845fd20f1f765d39
DanielD <danidiaza@gmail.com>
2016-11-23 02:22:37 -0600

    Pruebas finales

--- main.cpp
+++ main.cpp
@@ -33,27 +33,57 @@ int main(int argc, char* argv[]) {
 	*/
 
 
-	Graph g(35);
+	Graph g(50);
 
 	//
 	double r;
 	for (int i = 1; i < g.order(); i++) {
 		for (int j = 0; j < i; j++) {
 			r = ((double) rand() / (RAND_MAX));
-			(i-j <= (g.order()-2)) ? g.addEdgeBidir(i,j,r*100) : 0 ;
+			(i-j <= (g.order()*4/5)) ? g.addEdgeBidir(i,j,70+r*100) : 0 ;
 		}
 	}
 	//
+	
+	/*
+	// Camino mas corto!
+	// 0 -> 7 -> 3 -> 5 -> 11 -> 18 -> 19: 65.62
+	g.addEdgeBidir(0,7,5.2);
+	g.addEdgeBidir(7,3,1.9);
+	g.addEdgeBidir(3,5,11.4);
+	g.addEdgeBidir(5,11,16.4);
+	g.addEdgeBidir(11,18,8.92);
+	g.addEdgeBidir(18,19,22);
+	
+	// otro camino corto!
+	// 0 -> 15 -> 6 -> 3 -> 12 -> 19: 35.7 
+	g.addEdge(0,15,3.1);
+	g.addEdge(15,6,9.9);
+	g.addEdge(6,3,12.2);
+	g.addEdge(3,12,2.8);
+	g.addEdge(12,19,7.7);
+	*/
+	// 0 -> 7 -> 3 -> 12 -> 19 : 17.6
+	// 0 -> 15 -> 6 -> 3 -> 5 -> 11 -> 18 -> 19: 83.92
 
 	double v = g.order();
 	double e = g.size();
 	double D = e/(v*(v-1));
+	
 
 	cout << "Grafo con densidad " << D << endl;
 	g.print();
 
-	GeneticSolver s(5,5,5,&g);
+	int crossover = 4;
+	int mutaciones = 4;
+	int indel = 4;
+	int indelS = 4;
+	int gen_limit = 50;
+	int pool = 4;
+	GeneticSolver s(crossover, mutaciones, indel, indelS, gen_limit, pool, &g);
 
+	// ////////////////// //
+	// Pruebas Finales!!1 //
 	std::cout << "Resolviendo grafo" << std::endl;
 
 	s.solve();
@@ -72,8 +102,9 @@ int main(int argc, char* argv[]) {
 
 	std::cout << "Con un peso total de: " << s.genetic_solution.peso_total << std::endl;
 
-	GeneticSolver s2(5,5,10,&g);
+	//GeneticSolver s2(10,10,100,&g);
 
+	/*
 	std::cout << "Resolviendo grafo con mas generaciones" << std::endl;
 
 	s2.solve();
@@ -91,6 +122,7 @@ int main(int argc, char* argv[]) {
 	std::cout << std::endl;
 
 	std::cout << "Con un peso total de: " << s2.genetic_solution.peso_total << std::endl;
+	*/
 
 
 	/* Probar esSolucion
@@ -98,7 +130,7 @@ int main(int argc, char* argv[]) {
 	g.addEdgeBidir(0,5,950);
 	g.addEdgeBidir(14,5,123);
 	g.addEdgeBidir(0,2,101);
-	g.addEdgeBidir(2,3,154);
+	g.addEdgeBidir(2,3,15410);
 	g.addEdgeBidir(3,5,434);
 
 	g.addEdge(5,7,420);
@@ -156,7 +188,7 @@ int main(int argc, char* argv[]) {
 	*/
 
 	//
-	//
+	/*
 	cout << endl;
 	cout << "Intento de primera generacion" << endl;
 	s.primeraGeneracion();
@@ -166,7 +198,7 @@ int main(int argc, char* argv[]) {
 	}
 	cout << "Fin!" << endl;
 	cout << s.genepool.size() << " vectores generados" << endl;
-	//
+	*/
 
 // std::cout << "****************************************" << std::endl;
 // 	std::cout << "Llamando a seleccion natural" << std::endl;
@@ -187,6 +219,7 @@ int main(int argc, char* argv[]) {
 // 		cout << endl;
 // 	}
 
+	/*
 	cout << endl;
 	cout << "Crossover 1-1" << endl;
 	for (auto it = s.genepool.begin(); it != s.genepool.end(); it++) {
@@ -195,7 +228,7 @@ int main(int argc, char* argv[]) {
 		else
 			it--;
 	}
-	//
+	*/
 
 
 	/*
@@ -218,12 +251,32 @@ int main(int argc, char* argv[]) {
 	print(n3);
 	*/
 
+	// Probar INDEL
+	/*
+	Genoma g0,g1,g2,g3, n0,n1,n2,n3;
+	g0.genes = {0,7,8,3,4,5,9};
+	g1.genes = {0,2,9};
+	g2.genes = {0,9};
+	g3.genes = {0,6,2,9};
+
+	n0 = s.indel(g0);
+	n1 = s.indel(g1);
+	n2 = s.indel(g2);
+	n3 = s.indel(g3);
+	
+	print(n0);
+	print(n1);
+	print(n2);
+	print(n3);
+	*/
+
 	return 0;
 }
-
+/*
 void print(Genoma g) {
 	for (int i = 0; i < g.genes.size(); i++) {
 		std::cout << g.genes[i] << " ";
 	}
 	std::cout << "\tpeso: " << g.peso_total << endl;
 }
+*/
